b1ac921f4e7e9b7829236d6ed70fc9349c1a911c
kouzhengmin <1206960056@qq.com>
2021-06-19 22:22:57 +0800

    init

---
+++ myxmall-front-web/src/main/java/cn/kzm/MyxmallFrontWebApplication.java
@@ -0,0 +1,13 @@
+package cn.kzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyxmallFrontWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyxmallFrontWebApplication.class, args);
+    }
+
+}
